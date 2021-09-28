82aa02683d9c7a9dfcb26cf8caf143a734dfea35
TravisWongX <travis.x.wong@gmail.com>
2021-09-28 17:38:24 +0800

    完成sidebar组件样式

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-
+<div id="sidebar">
+  <div class="icons">
+    <router-link to="/note/1" title="笔记"><i class="iconfont icon-note"></i></router-link>
+    <router-link to="/notebooks" title="笔记本"><i class="iconfont icon-notebook"></i></router-link>
+    <router-link to="/trash/2" title="回收站" ><i class="iconfont icon-trash"></i></router-link>
+  </div>
+  <div class="logout">
+    <i class="iconfont icon-logout"></i>
+  </div>
   </div>
 </template>
 
@@ -16,5 +23,35 @@ export default class Sidebar extends Vue{
 
 
 <style lang="scss" scoped>
+#sidebar {
+  position: relative;
+  width: 56px;
+  text-align: center;
+  background-color: #2c333c;
 
+  > .icons {
+    margin-top: 15px;
+
+    > a {
+      padding: 6px 0;
+      display: block;
+    }
+
+    > router-link-active {
+      background: #5e6266;
+    }
+  }
+
+  .logout {
+    position: absolute;
+    bottom: 20px;
+    width: 100%;
+    text-align: center;
+    cursor: pointer;
+  }
+
+  .iconfont {
+    color: #fff;
+  }
+}
 </style>
