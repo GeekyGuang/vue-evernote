690816eb200af5803e73f846a78833a54d22135d
TravisWongX <travis.x.wong@gmail.com>
2021-10-18 18:33:47 +0800

    未登录跳转登录界面

--- src/components/NotebookList.vue
+++ src/components/NotebookList.vue
@@ -6,10 +6,19 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import Auth from '@/apis/auth'
 import {Component} from 'vue-property-decorator'
+
 @Component
 export default class NotebookList extends Vue{
-
+  created(){
+    Auth.get_info().then(res =>{
+      if(!res.isLogin){
+        this.$router.push('/login')
+      }
+    }
+    )
+  }
 }
 </script>
 <style lang="scss" scoped>
--- src/components/TrashDetail.vue
+++ src/components/TrashDetail.vue
@@ -7,8 +7,17 @@
 <script lang="ts">
 import Vue from 'vue'
 import {Component} from 'vue-property-decorator'
+import Auth from '@/apis/auth';
 @Component
 export default class TrashDetail extends Vue{
+  created() {
+    Auth.get_info().then(res => {
+          if (!res.isLogin) {
+            this.$router.push('/login')
+          }
+        }
+    )
+  }
 }
 </script>
 <style lang="scss" scoped>
